f0fef9c22ee7394514a8655eea008792ea881db8
Matheus Felipe Melo Costa Muniz <matheus.muniz.085@ufrn.edu.br>
2022-08-18 15:25:09 -0300

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-
+import React from "react";
+import logo from './assets/logo.png'
+import dashboard from './assets/dashboard.svg'
+import personagens from './assets/personagens.svg'
+import naves from './assets/nave.svg'
+import armas from './assets/armas.svg'
 function App() {
   return (
-    <div className="App">
-      <h1>teste projeto</h1>
+    <div className="container">
+      <div className="lateral-menu">
+        <img src={logo}/>
+        <div className="menu-itens">
+          <ul>
+            <li>
+              <img src={dashboard}/>
+              <p>Dashboard</p>
+            </li>
+            <li>
+              <img src={personagens}/>
+              <p>personagens</p>
+            </li>
+            <li>
+              <img src={naves}/>
+              <p>Naves</p>
+            </li>
+            <li>
+              <img src={armas}/>
+              <p>Armas</p>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
